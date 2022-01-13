cf81ab0ed8745b16aa9b635e6ea4c7b020992890
Lei Zhang <lei.zhang@amlogic.com>
2022-01-13 14:57:13 +0800

    c2: bringup c2 to console [1/1]
    PD#SWPL-70222
    
    Problem:
    C2 bringup to console basic
    
    Solution:
    C2 bringup to console basic
    
    Verify:
    C2 Af400
    
    Change-Id: Ic1b342875421740e595c2d9d32ff2999a3542813
    Signed-off-by: Lei Zhang <lei.zhang@amlogic.com>

---
+++ scripts/amlogic/mk_smarthome64.sh
@@ -0,0 +1,22 @@
+# SPDX-License-Identifier: (GPL-2.0+ OR MIT)
+#
+# Copyright (c) 2019 Amlogic, Inc. All rights reserved.
+#
+
+export CROSS_COMPILE=/opt/gcc-arm-10.2-2020.11-x86_64-aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-
+
+ROOTDIR=$(readlink -f $(dirname $0))/../../..
+OUTDIR=${ROOTDIR}/out/kernel-5.15/
+mkdir -p ${OUTDIR}/common
+
+DEFCONFIG=meson64_a64_smarthome_defconfig
+cp ${ROOTDIR}/common_drivers/arch/arm64/configs/${DEFCONFIG} ${ROOTDIR}/common/arch/arm64/configs/
+set -x
+make ARCH=arm64 -C ${ROOTDIR}/common O=${OUTDIR}/common ${DEFCONFIG}
+make ARCH=arm64 -C ${ROOTDIR}/common O=${OUTDIR}/common Image -j12
+make ARCH=arm64 -C ${ROOTDIR}/common O=${OUTDIR}/common modules -j12
+make ARCH=arm64 -C ${ROOTDIR}/common O=${OUTDIR}/common dtbs -j12
+rm ${ROOTDIR}/common/arch/arm64/configs/${DEFCONFIG}
+set +x
+
+echo "Build success!"

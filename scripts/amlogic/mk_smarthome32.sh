49f5c2ebbe83f763115095c1aabab78d0c562d7c
Jianxiong Pan <jianxiong.pan@amlogic.com>
2022-03-10 17:13:11 +0800

    mm: arm32: enable kasan. [1/1]
    PD#SWPL-73890
    
    Problem:
    enable kasan on arm32.
    
    Solution:
    enable CONFIG_KASAN config.
    
    Verify:
    local.
    
    Change-Id: I39c3459736c46b45f37c48d120b4e173fc614bdf
    Signed-off-by: Jianxiong Pan <jianxiong.pan@amlogic.com>

--- scripts/amlogic/mk_smarthome32.sh
+++ scripts/amlogic/mk_smarthome32.sh
@@ -8,7 +8,7 @@ ROOT_DIR=`pwd`
 
 ARCH=arm
 DEFCONFIG=meson64_a32_smarthome_defconfig
-CROSS_COMPILE_TOOL=/opt/gcc-linaro-7.3.1-2018.05-x86_64_armv8l-linux-gnueabihf/bin/armv8l-linux-gnueabihf-
+CROSS_COMPILE_TOOL=/opt/gcc-arm-8.3-2019.03-x86_64-arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
 MAKE='make'
 
 source ${ROOT_DIR}/common/common_drivers/scripts/amlogic/mk_smarthome_common.sh $@

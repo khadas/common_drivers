c4862895bf759c6bdc4a8011300281e12e88573d
ziyi <ziyi.huang@amlogic.com>
2022-06-10 15:08:22 +0800

    emmc: sc2 no mmc probe[1/1]
    PD#SWPL-84707
    
    Problem:
    no mmc probe
    
    Solution:
    enable emmc and sdcard in device tree
    
    Verify:
    sc2
    
    Change-Id: Ie0287f343407c1d294f80c813f99c491cdc5d50c
    Signed-off-by: ziyi <ziyi.huang@amlogic.com>

--- drivers/clk/meson/sc2.c
+++ drivers/clk/meson/sc2.c
@@ -4231,8 +4231,8 @@ static struct clk_regmap sc2_sd_emmc_b_clk0_sel = {
 		.ops = &clk_regmap_mux_ops,
 		.parent_data = sc2_sd_emmc_clk0_parent_data,
 		.num_parents = ARRAY_SIZE(sc2_sd_emmc_clk0_parent_data),
-		.flags = CLK_GET_RATE_NOCACHE | CLK_IGNORE_UNUSED |
-			CLK_SET_RATE_PARENT,
+		.flags = CLK_GET_RATE_NOCACHE | CLK_IGNORE_UNUSED,
+			//CLK_SET_RATE_PARENT,
 	},
 };
 
@@ -4249,8 +4249,8 @@ static struct clk_regmap sc2_sd_emmc_b_clk0_div = {
 			&sc2_sd_emmc_b_clk0_sel.hw
 		},
 		.num_parents = 1,
-		.flags = CLK_GET_RATE_NOCACHE | CLK_IGNORE_UNUSED |
-			CLK_SET_RATE_PARENT,
+		.flags = CLK_GET_RATE_NOCACHE | CLK_IGNORE_UNUSED,
+			//CLK_SET_RATE_PARENT,
 	},
 };
 
@@ -4266,8 +4266,8 @@ static struct clk_regmap sc2_sd_emmc_b_clk0 = {
 			&sc2_sd_emmc_b_clk0_div.hw
 		},
 		.num_parents = 1,
-		.flags = CLK_GET_RATE_NOCACHE | CLK_IGNORE_UNUSED |
-			CLK_SET_RATE_PARENT,
+		.flags = CLK_GET_RATE_NOCACHE | CLK_IGNORE_UNUSED,
+			//CLK_SET_RATE_PARENT,
 	},
 };
 

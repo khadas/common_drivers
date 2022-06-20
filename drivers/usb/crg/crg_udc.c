2b7c75892a1f4fa6b3e5498520233a6cc6f9afec
he.he <he.he@amlogic.com>
2022-06-20 13:51:33 +0800

    usb: c3 eye diagram test fail [1/1]
    PD#SWPL-85471
    
    Problem:
    c3 eye diagram test fail for package and
    test pass in buildroot
    
    Solution:
    Don't disconnect udc during in test mode
    
    Verify:
    c3
    
    Change-Id: I3b9b93400cc959f24ff0f00465d5b0b283ef02c8
    Signed-off-by: he.he <he.he@amlogic.com>

--- drivers/usb/crg/crg_udc.c
+++ drivers/usb/crg/crg_udc.c
@@ -3986,7 +3986,11 @@ int crg_handle_port_status(struct crg_gadget_dev *crg_udc)
 				ccs_drop_ignore = 1;
 				CRG_ERROR("ccs glitch detect on HS/FS!\n");
 			}
-
+#if IS_ENABLED(CONFIG_AMLOGIC_USB)
+			/**eye diagram test fail **/
+			if (CRG_U3DC_PORTSC_PLS_GET(tmp) == 0xb)
+				ccs_drop_ignore = 1;
+#endif
 			if (!ccs_drop_ignore)
 				crg_udc->gadget.speed = USB_SPEED_UNKNOWN;
 

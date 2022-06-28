3ed03c5332838473942a1bf9d110d911ff198ceb
Bichao Zheng <bichao.zheng@amlogic.com>
2022-06-28 15:36:57 +0800

    nand: fix rsv read fail [1/2]
    PD#SWPL-86523
    
    Problem:
    rsv read fail when bit flip
    
    Solution:
    Use mtd api instead
    
    Verify:
    c3
    
    Signed-off-by: Bichao Zheng <bichao.zheng@amlogic.com>
    Change-Id: I4fc9c94b2e47235c7a7d5a1c5fd592d84223f0ab

--- drivers/mtd/spinand/core.c
+++ drivers/mtd/spinand/core.c
@@ -1290,6 +1290,7 @@ static int spinand_init(struct spinand_device *spinand)
 	/* Propagate ECC information to mtd_info */
 	mtd->ecc_strength = nanddev_get_ecc_conf(nand)->strength;
 	mtd->ecc_step_size = nanddev_get_ecc_conf(nand)->step_size;
+	mtd->bitflip_threshold = mtd->ecc_strength;
 
 	return 0;
 

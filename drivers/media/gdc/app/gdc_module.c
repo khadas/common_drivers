9d83c5d4a40c30f4098c7f9032fd44109313690b
yuhua.lin <yuhua.lin@amlogic.com>
2022-09-16 19:19:27 +0800

    amlgdc: support amlgdc for kernel5.15 T7C [1/1]
    PD#SWPL-94184
    
    Problem:
    need to support amlgdc for kernel5.15 T7C
    
    Solution:
    complete it
    
    Verify:
    t7c
    
    Signed-off-by: yuhua.lin <yuhua.lin@amlogic.com>
    Change-Id: I10690c181ba22f503915473053c375304c1b69c7

--- drivers/media/gdc/app/gdc_module.c
+++ drivers/media/gdc/app/gdc_module.c
@@ -92,6 +92,15 @@ static struct gdc_device_data_s aml_gdc_v3 = {
 	.fw_version = AMLGDC_FW_V2
 };
 
+static struct gdc_device_data_s aml_gdc_v4 = {
+	.dev_type = AML_GDC,
+	.clk_type = MUXGATE_MUXSEL_GATE,
+	.bit_width_ext = 1,
+	.gamma_support = 0,
+	.core_cnt = 1,
+	.fw_version = AMLGDC_FW_V2
+};
+
 static const struct of_device_id gdc_dt_match[] = {
 	{.compatible = "amlogic, g12b-gdc", .data = &arm_gdc_clk2},
 	{.compatible = "amlogic, arm-gdc",  .data = &arm_gdc},
@@ -103,6 +112,7 @@ static const struct of_device_id amlgdc_dt_match[] = {
 	{.compatible = "amlogic, aml-gdc",  .data = &aml_gdc},
 	{.compatible = "amlogic, aml-gdc-v2",  .data = &aml_gdc_v2},
 	{.compatible = "amlogic, aml-gdc-v3",  .data = &aml_gdc_v3},
+	{.compatible = "amlogic, aml-gdc-v4",  .data = &aml_gdc_v4},
 	{} };
 
 MODULE_DEVICE_TABLE(of, amlgdc_dt_match);

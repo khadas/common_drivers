0cf8dd1a8dc7ca8191acc47549bed8822ce71376
lizhi.hu <lizhi.hu@amlogic.com>
2022-10-28 17:20:32 +0800

    lcd: fix phy set coding style [1/1]
    PD#SWPL-97537
    
    Problem:
    previous commit about phy set is ugly.
    
    Solution:
    replace with enum.
    
    Verify:
    t7c
    
    Signed-off-by: lizhi.hu <lizhi.hu@amlogic.com>
    Change-Id: I4fcba90fded92615741d5cc0f1b1c936257e4141

--- drivers/media/vout/lcd/lcd_common.h
+++ drivers/media/vout/lcd/lcd_common.h
@@ -18,6 +18,7 @@
 /* 20220622: c3 support bt656/1120*/
 /* 20220916: port k5.4 code to k5.15*/
 /* 20221012: correct t5w vbyone reset reg*/
+/* 20221028: fix lane lock && fix t7 mipi lprx reg set*/
 /* 20221115: support force unfit mipi-dsi bit_rate_max*/
 /* 20221116: add pinmux lock for c3*/
 #define LCD_DRV_VERSION    "20221116"

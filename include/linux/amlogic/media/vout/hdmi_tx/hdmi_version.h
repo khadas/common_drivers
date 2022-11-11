9d4b1f31ebe4bb6e523facf3db3a66ee91ee5120
hang cheng <hang.cheng@amlogic.com>
2022-11-11 15:25:44 +0800

    hdmitx: remove audio mute when adaptive from sdr to hdr [1/1]
    PD#SWPL-99719
    
    Problem:
    when switch from SDR to HDR under adaptive
    mode, audio will be mute for a few frames
    cause nts detect audio drops
    
    Solution:
    remove audio mute when adaptive from sdr to hdr
    
    Verify:
    ah212
    
    Test:
    test on ah212, start play hdr movie
    
    Change-Id: Ie09c09fa989a8c54f0a8ad688b8bc02ea51a9307
    Signed-off-by: hang cheng <hang.cheng@amlogic.com>

--- include/linux/amlogic/media/vout/hdmi_tx/hdmi_version.h
+++ include/linux/amlogic/media/vout/hdmi_tx/hdmi_version.h
@@ -40,6 +40,7 @@
 	"V03.00.02.00.01.03.10 [20221025] [COM] when set mode 4x3 and 16x9, return valid mode 1\n" \
 	"V03.00.02.00.01.03.11 [20221031] [NEW] add new format 2560x1440p60hz\n" \
 	"V03.00.02.00.01.04.11 [20221102] [EDID] adjust edid parsing for tvts\n" \
-	"V03.00.02.00.01.04.12 [20221111] [HPD] add hpd GPI status\n"
+	"V03.00.02.00.01.04.12 [20221111] [HPD] add hpd GPI status\n" \
+	"V03.00.02.00.01.04.13 [20221117] [BUG] remove audio mute when adaptive from sdr to hdr\n"
 
 #endif // __HDMI20_VERSION_H__

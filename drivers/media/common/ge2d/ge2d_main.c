35d6a2ecf1b1f3834fc6cb95c994135eeb18f239
yuhua.lin <yuhua.lin@amlogic.com>
2022-11-22 13:53:48 +0800

    ge2d: t5m ge2d bring up [1/1]
    PD#SWPL-98157
    
    Problem:
    t5m ge2d bring up
    
    Solution:
    complete it
    
    Verify:
    ptm
    
    Change-Id: Ib9fe972bc54a46dbf295df909099630cf60ea615
    Signed-off-by: yuhua.lin <yuhua.lin@amlogic.com>

--- drivers/media/common/ge2d/ge2d_main.c
+++ drivers/media/common/ge2d/ge2d_main.c
@@ -1482,6 +1482,22 @@ static struct ge2d_device_data_s ge2d_c3 = {
 	.cmd_queue_mode = 1,
 };
 
+static struct ge2d_device_data_s ge2d_t5m = {
+	.ge2d_rate = 500000000,
+	.src2_alp = 1,
+	.canvas_status = 2,
+	.deep_color = 1,
+	.hang_flag = 1,
+	.fifo = 1,
+	.has_self_pwr = 0,
+	.chip_type = MESON_CPU_MAJOR_ID_T5M,
+	.adv_matrix = 1,
+	.src2_repeat = 1,
+	.dst_repeat = 1,
+	.dst_sign_mode = 1,
+	.blk_stride_mode = 1,
+};
+
 static const struct of_device_id ge2d_dt_match[] = {
 #ifndef CONFIG_AMLOGIC_REMOVE_OLD
 	{
@@ -1552,6 +1568,10 @@ static const struct of_device_id ge2d_dt_match[] = {
 	{
 		.compatible = "amlogic, ge2d-c3",
 		.data = &ge2d_c3,
+	},
+	{
+		.compatible = "amlogic, ge2d-t5m",
+		.data = &ge2d_t5m,
 	},
 	{},
 };

bb37a672c4bbd361bcf65a679c4f9b09f0077635
binbin.wang <binbin.wang@amlogic.com>
2022-12-01 11:29:47 +0800

    Thermal: Kernel panic when entering an illegal value  [1/1]
    PD#SWPL-102920
    
    Problem:
    Because of wrong logic when writing an invalid value,
    the array is out of bounds
    
    Solution:
    Fix the wrong logic.
    
    Verify:
    T3
    
    Change-Id: I01216d799201f49de42802d78a5297a0cb0e74d0
    Signed-off-by: binbin.wang <binbin.wang@amlogic.com>

--- drivers/thermal/ddr_cooling.c
+++ drivers/thermal/ddr_cooling.c
@@ -18,8 +18,6 @@ static DEFINE_MUTEX(cooling_ddr_lock);
 static DEFINE_MUTEX(cooling_list_lock);
 static LIST_HEAD(ddr_dev_list);
 
-static void __iomem *ddr_reg0;
-
 static void ddr_coolingdevice_id_get(int *id)
 {
 	mutex_lock(&cooling_ddr_lock);
@@ -46,16 +44,53 @@ static int ddr_get_max_state(struct thermal_cooling_device *cdev,
 static int ddr_get_cur_state(struct thermal_cooling_device *cdev,
 			     unsigned long *state)
 {
-	*state = readl_relaxed(ddr_reg0);
+	struct ddr_cooling_device *ddr_device = cdev->devdata;
+	u32 val, mask;
+	u32 start_bit, end_bit;
+	int i;
+
+	start_bit = ddr_device->ddr_bits[0];
+	end_bit = ddr_device->ddr_bits[1];
+	mask = GENMASK(end_bit, start_bit);
+	val = readl_relaxed(ddr_device->vddr_reg);
+	val = (val & mask) >> start_bit;
+
+	for (i = 0; i < ddr_device->ddr_status; i++) {
+		if (val == ddr_device->ddr_data[i])
+			break;
+	}
+
+	if (i == ddr_device->ddr_status) {
+		dev_warn(&ddr_device->cool_dev->device,
+				"Not matching any states!\n");
+		*state = -EINVAL;
+	} else {
+		*state = i;
+	}
+
 	return 0;
 }
 
 static int ddr_set_cur_state(struct thermal_cooling_device *cdev,
 			     unsigned long state)
 {
+	struct ddr_cooling_device *ddr_device = cdev->devdata;
+	u32 val, mask;
+	u32 start_bit, end_bit;
 
-	if (state)
-		writel_relaxed(state, ddr_reg0);
+	if (!(state < ddr_device->ddr_status)) {
+		dev_warn(&ddr_device->cool_dev->device,
+				"Not matching any states!\n");
+		return -EINVAL;
+	}
+
+	start_bit = ddr_device->ddr_bits[0];
+	end_bit = ddr_device->ddr_bits[1];
+	mask = GENMASK(end_bit, start_bit);
+	val = readl_relaxed(ddr_device->vddr_reg);
+	val = (val & (~mask)) | (ddr_device->ddr_data[state] << start_bit);
+
+	writel_relaxed(val, ddr_device->vddr_reg);
 
 	return 0;
 }
@@ -68,41 +103,27 @@ static unsigned long cdev_calc_next_state_by_temp(struct thermal_instance *insta
 	struct thermal_cooling_device *cdev;
 	struct ddr_cooling_device *ddr_device;
 	int i, hyst = 0, trip_temp;
-	unsigned int val, reg_val, reg_val_ori;
 
 	if (!ins)
-		return 0;
+		return -EINVAL;
 
 	tz = ins->tz;
 	cdev = ins->cdev;
 
 	if (!tz || !cdev)
-		return 0;
+		return -EINVAL;
 
 	ddr_device = cdev->devdata;
 
 	tz->ops->get_trip_hyst(tz, instance->trip, &hyst);
 	tz->ops->get_trip_temp(tz, instance->trip, &trip_temp);
 
-	reg_val = readl_relaxed(ddr_reg0);
-	reg_val_ori = reg_val;
-
-	val = ddr_device->ddr_data[0];
-
-	for (i = 1; i < ddr_device->ddr_status; i++) {
+	for (i = ddr_device->ddr_status - 1; i > 0; i--) {
 		if (temperature >= (trip_temp + i * hyst))
-			val = ddr_device->ddr_data[i];
-		else
 			break;
 	}
-	pr_debug("chip temp: %d, set ddr reg bit val: %x\n", temperature, val);
 
-	reg_val &= ddr_device->ddr_bits_keep;
-	reg_val |= (val << ddr_device->ddr_bits[0]);
-	pr_debug("[%s %d][%d 0x%x][0x%x 0x%x]\n", __func__, __LINE__, temperature, reg_val,
-			reg_val_ori, val);
-
-	return reg_val;
+	return i;
 }
 
 static int ddr_get_requested_power(struct thermal_cooling_device *cdev,
@@ -136,61 +157,6 @@ static int ddr_power2state(struct thermal_cooling_device *cdev,
 	return 0;
 }
 
-static int ddr_notify_state(void *thermal_instance,
-			    int trip,
-			    enum thermal_trip_type type)
-{
-	struct thermal_instance *ins = thermal_instance;
-	struct thermal_zone_device *tz;
-	struct thermal_cooling_device *cdev;
-	struct ddr_cooling_device *ddr_device;
-	int i, hyst = 0, trip_temp;
-	unsigned int val, val0, reg_val;
-
-	if (!ins)
-		return -EINVAL;
-
-	tz = ins->tz;
-	cdev = ins->cdev;
-
-	if (!tz || !cdev)
-		return -EINVAL;
-
-	ddr_device = cdev->devdata;
-
-	tz->ops->get_trip_hyst(tz, trip, &hyst);
-	tz->ops->get_trip_temp(tz, trip, &trip_temp);
-
-	reg_val = readl_relaxed(ddr_reg0);
-	switch (type) {
-	case THERMAL_TRIP_HOT:
-		val = ddr_device->ddr_data[0];
-
-		for (i = 1; i < ddr_device->ddr_status; i++) {
-			if (tz->temperature >= (trip_temp + i * hyst))
-				val = ddr_device->ddr_data[i];
-			else
-				break;
-		}
-		pr_debug("chip temp: %d, set ddr reg bit val: %x\n", tz->temperature, val);
-
-		val0 = reg_val & ddr_device->ddr_bits_keep;
-		val0 = val0 >> ddr_device->ddr_bits[0];
-
-		if (val0 == val)
-			break;
-
-		reg_val &= ddr_device->ddr_bits_keep;
-		reg_val |= (val << ddr_device->ddr_bits[0]);
-		pr_debug("last set ddr reg val: %x\n", reg_val);
-
-		writel_relaxed(reg_val, ddr_reg0);
-	default:
-		break;
-	}
-	return 0;
-}
-
 static struct thermal_cooling_device_ops const ddr_cooling_ops = {
 	.get_max_state = ddr_get_max_state,
 	.get_cur_state = ddr_get_cur_state,
@@ -209,15 +175,14 @@ ddr_cooling_register(struct device_node *np, struct cool_dev *cool)
 	char dev_name[THERMAL_NAME_LENGTH];
 	int i;
 
-	(void) ddr_notify_state;
 	ddr_dev = kmalloc(sizeof(*ddr_dev), GFP_KERNEL);
 	if (!ddr_dev)
 		return ERR_PTR(-ENOMEM);
 
 	ddr_coolingdevice_id_get(&ddr_dev->id);
 
-	ddr_reg0 = ioremap(cool->ddr_reg, 1);
-	if (!ddr_reg0) {
+	ddr_dev->vddr_reg = ioremap(cool->ddr_reg, 4);
+	if (!ddr_dev->vddr_reg) {
 		pr_err("thermal ddr cdev: ddr reg0 ioremap fail.\n");
 		ddr_coolingdevice_id_put();
 		kfree(ddr_dev);
@@ -275,9 +240,8 @@ void ddr_cooling_unregister(struct thermal_cooling_device *cdev)
 	if (!cdev)
 		return;
 
-	iounmap(ddr_reg0);
-
 	ddr_dev = cdev->devdata;
+	iounmap(ddr_dev->vddr_reg);
 
 	mutex_lock(&cooling_list_lock);
 	list_del(&ddr_dev->node);

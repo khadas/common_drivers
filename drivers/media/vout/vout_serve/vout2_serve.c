d015c961d5b6215e50245a7f3373dbe93c1ec9ee
Evoke Zhang <evoke.zhang@amlogic.com>
2022-12-27 18:43:52 +0800

    vout: remove unused vpu_clkc [1/1]
    PD#SWPL-106957
    
    Problem:
    vou2 vpu_clkc is unused now
    
    Solution:
    remove vpu_clkc error print
    
    Verify:
    ar301
    
    Change-Id: I25f5a7a63ce1d561024ff4065f2273bb14be7f4e
    Signed-off-by: Evoke Zhang <evoke.zhang@amlogic.com>

--- drivers/media/vout/vout_serve/vout2_serve.c
+++ drivers/media/vout/vout_serve/vout2_serve.c
@@ -57,8 +57,6 @@ static unsigned int bist_mode2;
 
 static struct vout_cdev_s *vout2_cdev;
 static struct device *vout2_dev;
-static struct clk *vpu_clkc;
-static unsigned char vpu_clkc_state;
 
 /* **********************************************************
  * null display support
@@ -318,18 +316,6 @@ static int set_vout2_init_mode(void)
 		vmode = vout2_init_vmode | VMODE_INIT_BIT_MASK;
 	else
 		vmode = vout2_init_vmode;
-
-	if ((vmode & VMODE_MODE_BIT_MASK) < VMODE_NULL) {
-		if (IS_ERR_OR_NULL(vpu_clkc)) {
-			VOUTERR("vout2: vpu_clkc\n");
-		} else {
-			if (vpu_clkc_state == 0) {
-				VOUTPR("vout2: enable vpu_clkc\n");
-				clk_prepare_enable(vpu_clkc);
-				vpu_clkc_state = 1;
-			}
-		}
-	}
 
 	ret = set_current_vmode2(vmode);
 	if (ret) {
@@ -933,111 +919,6 @@ static void aml_vout2_late_resume(struct early_suspend *h)
  **	vout driver interface
  **
  ******************************************************************/
-static int vout2_clk_on_notifier(struct notifier_block *nb,
-				 unsigned long event, void *data)
-{
-	int *vmod;
-
-	if ((event & VOUT_EVENT_MODE_CHANGE_PRE) == 0)
-		return NOTIFY_DONE;
-
-	if (!data) {
-		VOUTERR("%s: data is NULL\n", __func__);
-		return NOTIFY_DONE;
-	}
-	vmod = (int *)data;
-	if (*vmod < VMODE_NULL) {
-		if (IS_ERR_OR_NULL(vpu_clkc)) {
-			VOUTERR("vout2: vpu_clkc\n");
-		} else {
-			if (vpu_clkc_state == 0) {
-				VOUTPR("vout2: enable vpu_clkc\n");
-				clk_prepare_enable(vpu_clkc);
-				vpu_clkc_state = 1;
-			}
-		}
-	}
-
-	return NOTIFY_OK;
-}
-
-static struct notifier_block vout2_clk_on_nb = {
-	.notifier_call = vout2_clk_on_notifier,
-};
-
-static int vout2_clk_off_notifier(struct notifier_block *nb,
-				  unsigned long event, void *data)
-{
-	int *vmod;
-
-	if ((event & VOUT_EVENT_MODE_CHANGE) == 0)
-		return NOTIFY_DONE;
-
-	if (!data) {
-		VOUTERR("%s: data is NULL\n", __func__);
-		return NOTIFY_DONE;
-	}
-	vmod = (int *)data;
-	if (*vmod >= VMODE_NULL) {
-		if (IS_ERR_OR_NULL(vpu_clkc)) {
-			VOUTERR("vout2: vpu_clkc\n");
-		} else {
-			if (vpu_clkc_state) {
-				VOUTPR("vout2: disable vpu_clkc\n");
-				clk_disable_unprepare(vpu_clkc);
-				vpu_clkc_state = 0;
-			}
-		}
-	}
-
-	return NOTIFY_OK;
-}
-
-static struct notifier_block vout2_clk_off_nb = {
-	.notifier_call = vout2_clk_off_notifier,
-};
-
-static int vout2_notifier_register(void)
-{
-	int ret = 0;
-
-	ret = vout2_register_client(&vout2_clk_on_nb);
-	if (ret)
-		VOUTERR("register vout2_clk_on_nb failed\n");
-	ret = vout2_register_client(&vout2_clk_off_nb);
-	if (ret)
-		VOUTERR("register vout2_clk_off_nb failed\n");
-
-	return 0;
-}
-
-static void vout2_notifier_unregister(void)
-{
-	vout2_unregister_client(&vout2_clk_on_nb);
-	vout2_unregister_client(&vout2_clk_off_nb);
-}
-
-static void vout2_clktree_init(struct device *dev)
-{
-	struct clk *vpu_clkc0;
-
-	vpu_clkc = NULL;
-	vpu_clkc_state = 0;
-
-	/* init & enable vpu_clk */
-	vpu_clkc0 = devm_clk_get(dev, "vpu_clkc0");
-	vpu_clkc = devm_clk_get(dev, "vpu_clkc");
-	if ((IS_ERR_OR_NULL(vpu_clkc0)) ||
-	    (IS_ERR_OR_NULL(vpu_clkc))) {
-		VOUTERR("vout2: %s: vpu_clkc failed\n", __func__);
-	} else {
-		WARN_ON(clk_set_rate(vpu_clkc0, 200000000));
-		WARN_ON(clk_set_parent(vpu_clkc, vpu_clkc0));
-	}
-
-	pr_debug("vout2: clktree_init\n");
-}
-
 static void aml_vout2_get_dt_info(struct platform_device *pdev)
 {
 	int ret;
@@ -1072,9 +953,7 @@ static int aml_vout2_probe(struct platform_device *pdev)
 	ret = vout2_attr_create();
 	ret = vout2_fops_create();
 
-	vout2_clktree_init(&pdev->dev);
 	vout2_register_server(&nulldisp_vout2_server);
-	vout2_notifier_register();
 
 	set_vout2_init_mode();
 
@@ -1087,7 +966,6 @@ static int aml_vout2_remove(struct platform_device *pdev)
 #ifdef CONFIG_AMLOGIC_LEGACY_EARLY_SUSPEND
 	unregister_early_suspend(&early_suspend);
 #endif
-	vout2_notifier_unregister();
 
 	vout2_attr_remove();
 	vout2_fops_remove();

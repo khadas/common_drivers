6f1f1faad1bd9d7a5e4b36c6e9665252c6c9d758
Evoke Zhang <evoke.zhang@amlogic.com>
2023-01-05 10:53:23 +0800

    lcd: update clk ss support [1/1]
    PD#SWPL-107284
    
    Problem:
    lcd clk ss freq and mode parameters are invalid
    
    Solution:
    update clk ss support
    
    Verify:
    at301
    
    Change-Id: I9d2c51021de20278dfdf70a9f360c83cf2fda2a5
    Signed-off-by: Evoke Zhang <evoke.zhang@amlogic.com>

--- drivers/media/vout/lcd/lcd_common.h
+++ drivers/media/vout/lcd/lcd_common.h
@@ -26,7 +26,8 @@
 /* 20221208: remove black pattern when enable*/
 /* 20221215: remove unnecessary tcon top reset*/
 /* 20221216: optimize lcd clk code*/
-#define LCD_DRV_VERSION    "20221216"
+/* 20230105: update clk ss support*/
+#define LCD_DRV_VERSION    "20230105"
 
 extern struct mutex lcd_vout_mutex;
 extern spinlock_t lcd_reg_spinlock;
@@ -152,11 +153,11 @@ int lcd_debug_info_len(int num);
 void lcd_clk_generate_parameter(struct aml_lcd_drv_s *pdrv);
 
 int lcd_get_ss(struct aml_lcd_drv_s *pdrv, char *buf);
-void lcd_clk_ss_config_update(struct aml_lcd_drv_s *pdrv);
-int lcd_set_ss(struct aml_lcd_drv_s *pdrv, unsigned int level,
-				unsigned int freq, unsigned int mode);
 int lcd_get_ss_num(struct aml_lcd_drv_s *pdrv,
 	unsigned int *level, unsigned int *freq, unsigned int *mode);
+int lcd_set_ss(struct aml_lcd_drv_s *pdrv, unsigned int level,
+				unsigned int freq, unsigned int mode);
+void lcd_clk_ss_config_init(struct aml_lcd_drv_s *pdrv);
 int lcd_encl_clk_msr(struct aml_lcd_drv_s *pdrv);
 void lcd_pll_reset(struct aml_lcd_drv_s *pdrv);
 void lcd_update_clk(struct aml_lcd_drv_s *pdrv);

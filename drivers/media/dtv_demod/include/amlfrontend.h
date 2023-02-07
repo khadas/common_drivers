e53f2272cf6e7256730ae56138ce89530c9d1c6f
zhicheng.huang <zhicheng.huang@amlogic.com>
2023-02-07 07:54:34 +0000

    dtv_demod: fix dvbc low sr and 16/32qam unlock[V1.1.77] [1/1]
    PD#SWPL-108405
    
    Problem:
    fix dvbc low sr and 16/32qam unlock.
    
    Solution:
    fix dvbc low sr and 16/32qam unlock.
    for r836 tuner, 16/32qam need to wait more time.
    AMLDTVDEMOD_VER "V1.1.77".
    
    Verify:
    s4d
    
    Change-Id: I096aa43a2b324daaa0aaa0e790135cfd78784b6f
    Signed-off-by: zhicheng.huang <zhicheng.huang@amlogic.com>

--- drivers/media/dtv_demod/include/amlfrontend.h
+++ drivers/media/dtv_demod/include/amlfrontend.h
@@ -68,6 +68,7 @@
 /*  V1.1.74  fix s ber and adc fga gain and atsc ch if */
 /*  V1.1.75  optimize identification of auto qam */
 /*  V1.1.76  fix t5m DVB-C auto QAM timeout */
+/*  V1.1.77  fix dvbc low sr and 16/32qam unlock */
 /****************************************************/
 /****************************************************************/
 /*               AMLDTVDEMOD_VER  Description:                  */
@@ -84,8 +85,8 @@
 /*->The last four digits indicate the release time              */
 /****************************************************************/
 #define KERNEL_4_9_EN		1
-#define AMLDTVDEMOD_VER "V1.1.76"
-#define DTVDEMOD_VER	"2023/01/30: fix t5m DVB-C auto QAM timeout"
+#define AMLDTVDEMOD_VER "V1.1.77"
+#define DTVDEMOD_VER	"2023/2/1: fix dvbc low sr and 16/32qam unlock"
 #define AMLDTVDEMOD_T2_FW_VER "V1551.20220524"
 #define DEMOD_DEVICE_NAME  "dtvdemod"
 

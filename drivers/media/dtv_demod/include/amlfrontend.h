df3156a93322cf2905d3a4dcecdf614c5338a84c
nengwen.chen <nengwen.chen@amlogic.com>
2023-03-27 12:20:16 +0800

    dtv_demod: fix switch to T2 unlock (V1.1.89) [1/1]
    PD#SWPL-108238
    
    Problem:
    fix switch to T2 unlock.
    
    Solution:
    fix switch to T2 unlock.
    fix fifo adc in and ddr clock reset.
    AMLDTVDEMOD_VER "V1.1.89"
    
    Verify:
    verify by T5D/T3.
    
    Change-Id: I12f7497306b8298fe7b4b27f614ff3d6c5587ab7
    Signed-off-by: nengwen.chen <nengwen.chen@amlogic.com>

--- drivers/media/dtv_demod/include/amlfrontend.h
+++ drivers/media/dtv_demod/include/amlfrontend.h
@@ -80,6 +80,7 @@
 /*  V1.1.86  remove S/S2 ber config */
 /*  V1.1.87  fix t5m dvbc 128qam unlock */
 /*  V1.1.88  fix dvb-s unicable blind scan failed */
+/*  V1.1.89  fix T5D/T3 switch to T2 unlock */
 /****************************************************/
 /****************************************************************/
 /*               AMLDTVDEMOD_VER  Description:                  */
@@ -96,8 +97,8 @@
 /*->The last four digits indicate the release time              */
 /****************************************************************/
 #define KERNEL_4_9_EN		1
-#define AMLDTVDEMOD_VER "V1.1.88"
-#define DTVDEMOD_VER	"2023/3/14: fix dvb-s unicable blind scan failed"
+#define AMLDTVDEMOD_VER "V1.1.89"
+#define DTVDEMOD_VER	"2023/3/27: fix T5D/T3 switch to T2 unlock"
 #define AMLDTVDEMOD_T2_FW_VER "V1551.20220524"
 #define DEMOD_DEVICE_NAME  "dtvdemod"
 

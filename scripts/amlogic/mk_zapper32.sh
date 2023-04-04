1ff6652c7deb6383aab328906b440d049cf30f9e
Jianxiong Pan <jianxiong.pan@amlogic.com>
2023-04-04 14:57:25 +0800

    zapper: add zapper defconfig. [1/1]
    PD#SWPL-116862
    
    Problem:
    add zapper defconfig.
    
    Solution:
    get from mk_gx32.sh
    
    Verify:
    build pass.
    
    Change-Id: Ibaaf4095617e825d36d9657404ee7c6e897f6133
    Signed-off-by: Jianxiong Pan <jianxiong.pan@amlogic.com>

---
+++ scripts/amlogic/mk_zapper32.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# SPDX-License-Identifier: (GPL-2.0+ OR MIT)
+#
+# Copyright (c) 2019 Amlogic, Inc. All rights reserved.
+#
+
+ROOT_DIR=`pwd`
+
+ARCH=arm
+DEFCONFIG=meson64_a32_zapper_defconfig
+
+KCONFIG_CONFIG=${ROOT_DIR}/common/common_drivers/arch/${ARCH}/configs/${DEFCONFIG}
+
+export -n KCONFIG_CONFIG
+CROSS_COMPILE_TOOL=${ROOT_DIR}/prebuilts/gcc/linux-x86/host/x86_64-arm-10.3-2021.07/bin/arm-none-linux-gnueabihf-
+
+source ${ROOT_DIR}/common/common_drivers/scripts/amlogic/mk_smarthome_common.sh $@

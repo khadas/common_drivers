df78e3ac908392371abba1f133f9584443238cd9
Ao Xu <ao.xu@amlogic.com>
2023-04-04 15:14:32 +0800

    vout: enable t3x vs_measure [1/1]
    PD#SWPL-117229
    
    Problem:
    t3x vs_measure use dft measure api
    
    Solution:
    t3x use valid vs_measure api
    
    Verify:
    bc302
    
    Change-Id: I307d80bdbdf5b1b43b6d982aff83e186ca89cbea
    Signed-off-by: Ao Xu <ao.xu@amlogic.com>

--- drivers/media/vout/vout_serve/vout_mux.c
+++ drivers/media/vout/vout_serve/vout_mux.c
@@ -433,7 +433,7 @@ static const struct of_device_id vout_mux_dt_match_table[] = {
 	},
 	{
 		.compatible = "amlogic, vout_mux-t3x",
-		.data = &vout_mux_match_data_t3,
+		.data = &vout_mux_match_data_s5,
 	},
 #endif
 	{}

bf2e0ea64842aea37cf07981ad46c57d92b773d2
min.yang <min.yang@amlogic.com>
2023-05-16 14:17:34 +0800

    dtv_demod: fix dvb-s unicable blind scan (V3.1.101) [1/1]
    PD#SWPL-120467
    PD#SWPL-120814
    
    Problem:
    dvbs will miss TP when using unicable.
    
    Solution:
    1.remove resetting the voltage for H polarization.
    2.demod driver set LNB off while leaving DVBS mode.
    3.fix blind scan flow.
    
    Verify:
    verified by T5D.
    
    Change-Id: I45a76d54c0d2ad6bb53b0eebdf93fa86be5d4583
    Signed-off-by: min.yang <min.yang@amlogic.com>

--- drivers/media/dtv_demod/include/amlfrontend.h
+++ drivers/media/dtv_demod/include/amlfrontend.h
@@ -91,7 +91,8 @@
 /*  V1.1.97  optimize 8VSB CN */
 /*  V1.1.98  fix r842 atsc missing TP and scan slowly */
 /*  V1.1.99  fix init flow */
-/*  V1.2.0  bring up dump ADC data feature */
+/*  V1.1.100  bring up dump ADC data feature */
+/*  V3.1.101  fix dvb-s unicable blind scan miss TP */
 /****************************************************/
 /****************************************************************/
 /*               AMLDTVDEMOD_VER  Description:                  */
@@ -108,8 +109,8 @@
 /*->The last four digits indicate the release time              */
 /****************************************************************/
 #define KERNEL_4_9_EN		1
-#define AMLDTVDEMOD_VER "V1.2.0"
-#define DTVDEMOD_VER	"2023/5/9: bring up dump ADC data feature"
+#define AMLDTVDEMOD_VER "V3.1.101"
+#define DTVDEMOD_VER	"2023/5/16: fix dvb-s unicable blind scan miss TP"
 #define AMLDTVDEMOD_T2_FW_VER "V1551.20220524"
 #define DEMOD_DEVICE_NAME  "dtvdemod"
 

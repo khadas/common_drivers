f4d20c177a1683537b1837981e4b9a4c9561328e
shufei.zhao <shufei.zhao@amlogic.com>
2023-06-26 05:44:26 +0000

    unifykey: unifykey optimize [1/1]
    PD#SWPL-128302
    
    Problem:
    UnifyKey uniformly uses the
    open/read/write/ioctl/close
    interface in user space
    
    Solution:
    add lock
    
    Verify:
    T3X
    
    Change-Id: Ia08294724c256b6b926c35ab72a9a3cb16cd16a5
    Signed-off-by: shufei.zhao <shufei.zhao@amlogic.com>

--- drivers/efuse_unifykey/unifykey.c
+++ drivers/efuse_unifykey/unifykey.c
@@ -34,6 +34,7 @@ typedef int (*key_unify_dev_init)(struct key_info_t *uk_info,
 				  char *buf, unsigned int len);
 
 static struct aml_uk_dev *ukdev_global;
+static int lock;
 
 /*
  * This strange API is for LCD driver to call my internal APIs,
@@ -468,6 +469,11 @@ static int unifykey_open(struct inode *inode, struct file *file)
 {
 	struct aml_uk_dev *ukdev;
 
+	if (lock == 1) {
+		pr_err("%s: can not operate other key\n", __func__);
+		return -EPERM;
+	}
+	lock = 1;
 	ukdev = container_of(inode->i_cdev, struct aml_uk_dev, cdev);
 	file->private_data = ukdev;
 
@@ -476,6 +482,7 @@ static int unifykey_open(struct inode *inode, struct file *file)
 
 static int unifykey_release(struct inode *inode, struct file *file)
 {
+	lock = 0;
 	return 0;
 }
 
@@ -648,6 +655,7 @@ static ssize_t unifykey_read(struct file *file,
 		ret =  -EFAULT;
 		goto exit;
 	}
+	pr_info("%s: name: %s, size %d\n", __func__, item->name, (int)count);
 	ret = count;
 exit:
 	kfree(local_buf);
@@ -684,6 +692,7 @@ static ssize_t unifykey_write(struct file *file,
 	ret = key_unify_write(ukdev, item->name, local_buf, count);
 	if (ret < 0)
 		goto exit;
+	pr_info("%s: name: %s, size %d\n", __func__, item->name, (int)count);
 	ret = count;
 exit:
 	kfree(local_buf);

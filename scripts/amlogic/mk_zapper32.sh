b26b66acf9aae6e33d661750305ff3d347d6490b
yao zhang1 <yao.zhang1@amlogic.com>
2023-07-27 15:09:54 +0800

    config: Add zapper extra defconfig. [1/1]
    PD#SWPL-133035
    
    Problem:
    Need some debug configs & modules verification.
    
    Solution:
    Add zapper extra defconfig.
    
    Verify:
    s1a bg201/bg209
    
    Change-Id: I3e99dd9473ac7ae8e79086ebeab686c67e8efc33
    Signed-off-by: yao zhang1 <yao.zhang1@amlogic.com>

--- scripts/amlogic/mk_zapper32.sh
+++ scripts/amlogic/mk_zapper32.sh
@@ -7,11 +7,29 @@
 ROOT_DIR=`pwd`
 
 ARCH=arm
+if [ "$1" = "--all" ]; then
+DEFCONFIG=meson64_a32_zapper_all_defconfig
+else
 DEFCONFIG=meson64_a32_zapper_defconfig
+fi
 
 KCONFIG_CONFIG=${ROOT_DIR}/common/common_drivers/arch/${ARCH}/configs/${DEFCONFIG}
+
+if [ "$1" = "--all" ]; then
+export KCONFIG_CONFIG
+
+${ROOT_DIR}/common/scripts/kconfig/merge_config.sh -m -r \
+	${ROOT_DIR}/common/common_drivers/arch/${ARCH}/configs/meson64_a32_zapper_defconfig  \
+	${ROOT_DIR}/common/common_drivers/arch/${ARCH}/configs/meson64_a32_zapper_ext_defconfig  \
+
+fi
 
 export -n KCONFIG_CONFIG
 CROSS_COMPILE_TOOL=${ROOT_DIR}/prebuilts/gcc/linux-x86/host/x86_64-arm-10.3-2021.07/bin/arm-none-linux-gnueabihf-
 
 source ${ROOT_DIR}/common/common_drivers/scripts/amlogic/mk_smarthome_common.sh $@
+
+if [ "$1" = "--all" ]; then
+	rm ${KCONFIG_CONFIG}*
+fi
+

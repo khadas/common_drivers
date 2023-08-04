eb684e8e7ced3ee64d616069275825e21d17dc9b
qinglin.li <qinglin.li@amlogic.com>
2023-08-04 19:48:50 +0800

    script: typo issue [1/1]
    PD#SWPL-134886
    
    Problem:
    typo issue
    
    Solution:
    fix typo issue
    
    Verify:
    c3
    
    Change-Id: I586aeda313c287861fa10a357b1d882cc1ed721b
    Signed-off-by: qinglin.li <qinglin.li@amlogic.com>

--- scripts/amlogic/mk_c3_min.sh
+++ scripts/amlogic/mk_c3_min.sh
@@ -8,11 +8,11 @@ ROOT_DIR=`pwd`
 ARCH=arm
 CROSS_COMPILE_TOOL=${ROOT_DIR}/prebuilts/gcc/linux-x86/host/x86_64-arm-10.3-2021.07/bin/arm-none-linux-gnueabihf-
 
-OPRION_PARAM="$*"
-if [[ "${OPRION_PARAM}" =~ "--c3_debug" ]]; then
+OPTION_PARAM="$*"
+if [[ "${OPTION_PARAM}" =~ "--c3_debug" ]]; then
 	echo "make meson64_a32_C3_mini_defconfig & C3_debug_defconfig"
 	DEBUG=1
-	OPRION_PARAM=${OPRION_PARAM//--c3_debug/}
+	OPTION_PARAM=${OPTION_PARAM//--c3_debug/}
 	DEFCONFIG=meson64_a32_C3_mini_merge_defconfig
 	CONFIG_DIR=${ROOT_DIR}/common/common_drivers/arch/${ARCH}/configs
 	KCONFIG_CONFIG=${CONFIG_DIR}/${DEFCONFIG} ${ROOT_DIR}/common/scripts/kconfig/merge_config.sh -m -r \
@@ -23,6 +23,8 @@ else
 	DEFCONFIG=meson64_a32_C3_mini_defconfig
 fi
 
-source ${ROOT_DIR}/common/common_drivers/scripts/amlogic/mk_smarthome_common.sh ${OPRION_PARAM}
+source ${ROOT_DIR}/common/common_drivers/scripts/amlogic/mk_smarthome_common.sh ${OPTION_PARAM}
 
-[[ "${DEBUG}" == "1" ]] && rm -f ${CONFIG_DIR}/${DEFCONFIG}*
+if [[ "${DEBUG}" == "1" ]]; then
+	rm -f ${CONFIG_DIR}/${DEFCONFIG}*
+fi

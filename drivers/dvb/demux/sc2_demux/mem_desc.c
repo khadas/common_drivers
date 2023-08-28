103e85bb3b9efd5e62fc4c8466b7a7a23614e216
hongyu.chen <hongyu.chen@amlogic.com>
2023-08-28 16:40:36 +0800

    demux: Increase the delay of writing data from 1ms to 1s. [1/1]
    PD#SWPL-133232
    
    Problem:
    The data was not written within 1ms.
    
    Solution:
    Increase delay from 1ms to 1s.
    
    Verify:
    S905X4/AH212
    
    Change-Id: I2facfbccf1e523f4748a956b3fe15a59037c0fad
    Signed-off-by: hongyu.chen <hongyu.chen@amlogic.com>

--- drivers/dvb/demux/sc2_demux/mem_desc.c
+++ drivers/dvb/demux/sc2_demux/mem_desc.c
@@ -1558,9 +1558,9 @@ int SC2_bufferid_write(struct chan_id *pchan, const char __user *buf,
 					   pchan->mem_size, total, pack_len);
 		}
 		prev_time_nsec = ktime_to_ns(ktime_get());
-		max_timeout_nsec = (s64)write_timeout_ms * 1000;
+		max_timeout_nsec = (s64)write_timeout_ms * 1000 * 1000;
 		do {
-			usleep_range(1, 10);
+			usleep_range(100, 200);
 		} while (!rdma_get_done(pchan->id) &&
 			(ktime_to_ns(ktime_get()) - prev_time_nsec) < max_timeout_nsec);
 

f9cabbe3101c7ab5d17e041c0b62e6137e521a06
lizhi.hu <lizhi.hu@amlogic.com>
2023-10-31 17:09:09 +0800

    lcd: t3x lcd1 pll id fix [1/1]
    PD#SWPL-144319
    
    Problem:
    t3x lcd1 pll id set to 0
    
    Solution:
    change t3x lcd1 pll id to 1
    
    Verify:
    t3x_bc302
    
    Change-Id: I3e8d0efeade2d02907a0497ed255de93c21fb72f
    Signed-off-by: lizhi.hu <lizhi.hu@amlogic.com>

--- drivers/media/vout/lcd/lcd_clk/lcd_clk_t3x.c
+++ drivers/media/vout/lcd/lcd_clk/lcd_clk_t3x.c
@@ -1051,6 +1051,7 @@ void lcd_clk_config_chip_init_t3x(struct aml_lcd_drv_s *pdrv, struct lcd_clk_con
 		cconf->pll_offset = (0x5 << 2);
 		cconf->pll_od_fb = cconf->data->pll_od_fb;
 		cconf->clk_path_change = NULL;
+		cconf->pll_id = 1;
 		break;
 	case 0:
 	default:

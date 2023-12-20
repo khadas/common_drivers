9c9a1d70e3af1130f6bfcddff7cb2729f6510b8f
Reeve Huang <reeve.huang@wesion.com>
2023-12-20 17:55:30 +0800

    amlisp: VIM4N: add OS08A10 MIPI camera support
    Signed-off-by: Reeve Huang <reeve.huang@wesion.com>
    Change-Id: I92b3a0799fe40eabc758d1c2eb27a8fca9213617

--- drivers/amlisp/amlsens/ov08a10/aml_ov08a10.c
+++ drivers/amlisp/amlsens/ov08a10/aml_ov08a10.c
@@ -551,12 +551,18 @@ int ov08a10_power_on(struct device *dev, struct sensor_gpio *gpio)
 	int ret;
 
 	gpiod_set_value_cansleep(gpio->rst_gpio, 1);
-	if (!IS_ERR_OR_NULL(gpio->pwdn_gpio)) {
-		gpiod_set_value_cansleep(gpio->pwdn_gpio, 1);
-	}
+	usleep_range(30000, 31000);
+
+	gpiod_set_value_cansleep(gpio->rst_gpio, 0);
+	usleep_range(100000, 110000);
+
+	gpiod_set_value_cansleep(gpio->rst_gpio, 1);
+	usleep_range(30000, 31000);
+
 	ret = mclk_enable(dev,24000000);
 	if (ret < 0 )
 		dev_err(dev, "set mclk fail\n");
+	udelay(30);
 
 	// 30ms
 	usleep_range(30000, 31000);
@@ -864,10 +870,10 @@ int ov08a10_sensor_id(struct i2c_client *client)
 	id |= val;
 
 	if (id != OV08A10_ID) {
-		dev_info(&client->dev, "Failed to get ov08a10 id: 0x%x\n", id);
+		dev_err(&client->dev, "Failed to get ov08a10 id: 0x%x\n", id);
 		return rtn;
 	} else {
-		dev_err(&client->dev, "success get ov08a10 id 0x%x", id);
+		dev_info(&client->dev, "success get ov08a10 id 0x%x", id);
 	}
 
 	return 0;
